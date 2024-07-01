4ae76230d2c69fcb0ee8f5ab86672ee13aad7820
WTCHAI <103483111+WTCHAI@users.noreply.github.com>
2024-07-01 15:35:17 +0700

    Add files via upload

---
+++ Bread.java
@@ -0,0 +1,8 @@
+
+public class Bread {
+    public String name ; 
+
+    public Bread(String name){
+        this.name = name ;
+    }
+}
---
+++ Ingredient.java
@@ -0,0 +1,9 @@
+public class Ingredient {
+    public String name ; 
+    public int calories ;
+
+    public Ingredient(String name, int calories){
+        this.name = name;
+        this.calories = calories;
+    }
+}
